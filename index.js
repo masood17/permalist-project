bbbd1213fdcb99c39f1a22c5c6fe7af83f2d74d8
masu <graphicgorillaz@gmail.com>
2024-03-13 13:12:24 +0200

    add public and views

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
