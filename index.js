adfee101a807e835d1c3ae3448d3bb5a7ebc36d7
masood17 <graphicgorillaz@gmail.com>
2024-03-16 20:34:59 +0200

    Create dotenv file, connect to postgresql db,

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
